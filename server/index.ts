036d8aaeea400959b1cd1ae57e670635b375721c
anton.maignan <anton.maignan@emeria.eu>
2026-07-11 15:03:11 +0200

    feat(server/users): add creation user route

--- server/index.ts
+++ server/index.ts
@@ -2,8 +2,10 @@ import Elysia from "elysia";
 import { config } from "./config";
 import { swagger } from "@elysiajs/swagger";
 import { reservationRoutes } from "./src/reservations/routes";
+import { userRoutes } from "./src/users/routes";
 
 export const app = new Elysia()
   .use(swagger())
   .use(reservationRoutes)
+  .use(userRoutes)
   .listen(config.PORT);
